f156641ae25f0b244ce480f55d55af5a5f30a31c
Moses583 <moisendungu@gmail.com>
2024-07-24 08:51:33 +0300

    Added Search bar to transactions, added editing offer, fixed dialogs

--- app/src/main/java/com/bingwa/bingwasokonibot/fragments/MainContentFragment.java
+++ app/src/main/java/com/bingwa/bingwasokonibot/fragments/MainContentFragment.java
@@ -77,14 +77,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class MainContentFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private DBHelper helper,helper2;
     List<TransactionPOJO> pojoList = new ArrayList<>();
 
@@ -121,10 +113,7 @@ public class MainContentFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
